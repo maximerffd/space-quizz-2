ec4546614f018ccc1ee575950f6db012a743e989
Mhyran <maxime.rffd@gmail.com>
2021-03-10 10:37:41 +0100

    Add unique constraint on username

--- app/src/main/java/fr/mhyran/spacequizz2/Controllers/RegisterActivity.java
+++ app/src/main/java/fr/mhyran/spacequizz2/Controllers/RegisterActivity.java
@@ -12,6 +12,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.mhyran.spacequizz2.Config.Constant;
 import fr.mhyran.spacequizz2.Database.AppDatabase;
 import fr.mhyran.spacequizz2.Entity.User;
@@ -19,10 +23,9 @@ import fr.mhyran.spacequizz2.Interfaces.UserDao;
 import fr.mhyran.spacequizz2.R;
 
 
-
-
-public class  RegisterActivity extends AppCompatActivity {
-
+public class RegisterActivity extends AppCompatActivity {
+    List<User> listUsernames;
+    boolean pseudoAlreadyUsed = false;
 
     AppDatabase db;
 
@@ -37,6 +40,8 @@ public class  RegisterActivity extends AppCompatActivity {
         EditText password = (EditText) findViewById(R.id.editTextTextPassword);
         Button registerButton = (Button) findViewById(R.id.registerButton);
 
+        listUsernames = new ArrayList<>();
+
 
         db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, Constant.BD_NAME)
                 .allowMainThreadQueries()
@@ -50,33 +55,47 @@ public class  RegisterActivity extends AppCompatActivity {
                 User obj = new User();
                 obj.setPseudo(username.getText().toString());
                 obj.setPassword(password.getText().toString());
-             if (validateInput(obj)) {
-                 AppDatabase sInstance = AppDatabase.getAppDatabase(getApplicationContext());
-                 UserDao userDao = sInstance.userDao();
-                 new Thread(new Runnable() {
-                     @Override
-                     public void run() {
-                    //     if(obj.getPseudo().equals(userDao.getAllUsersReg())) {
-                      //       Toast.makeText(RegisterActivity.this, "Pseudo déjà utilisé", Toast.LENGTH_SHORT).show();
-                         }else {
-                             userDao.registerUser(obj);
-                             runOnUiThread(new Runnable() {
-                                 @Override
-                                 public void run() {
-                                     Toast.makeText(RegisterActivity.this, "Utilisateur enregistré", Toast.LENGTH_SHORT).show();
-                                     Intent mainActivity = new Intent(RegisterActivity.this, MainActivity.class);
-                                     startActivity(mainActivity);
-                                 }
-                             });
-                         }
-                     }
-                 }).start();
-             }else{
-                 Toast.makeText(RegisterActivity.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
-             }
+                if (validateInput(obj)) {
+                    AppDatabase sInstance = AppDatabase.getAppDatabase(getApplicationContext());
+                    UserDao userDao = sInstance.userDao();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            pseudoAlreadyUsed = false;
+                            listUsernames.addAll(userDao.getAllUsersReg());
+                            System.out.println(userDao.getAllUsersReg());
+                            for (int i = 0; i < listUsernames.size(); i++) {
+                                if (obj.getPseudo().equals(listUsernames.get(i).getPseudo())) {
+                                    pseudoAlreadyUsed = true;
+                                }
+                            };
+                            if (pseudoAlreadyUsed) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(RegisterActivity.this, "Pseudo déjà utilisé", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            } else {
+                                userDao.registerUser(obj);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(RegisterActivity.this, "Utilisateur enregistré", Toast.LENGTH_SHORT).show();
+                                        Intent mainActivity = new Intent(RegisterActivity.this, MainActivity.class);
+                                        startActivity(mainActivity);
+                                    }
+                                });
+                            }
+                        }
+                    }).start();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
     private Boolean validateInput(User obj) {
         return !obj.getPseudo().isEmpty() &&
                 !obj.getPassword().isEmpty();
--- app/src/main/java/fr/mhyran/spacequizz2/Interfaces/UserDao.java
+++ app/src/main/java/fr/mhyran/spacequizz2/Interfaces/UserDao.java
@@ -1,50 +1,51 @@
 package fr.mhyran.spacequizz2.Interfaces;
 
 import android.database.Cursor;
+
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
+
 import java.util.List;
+
 import fr.mhyran.spacequizz2.Entity.User;
 
 @Dao
-public interface UserDao{
+public interface UserDao {
 
- @Query("SELECT COUNT(*) FROM " + User.TABLE_NAME)
- int count();
+    @Query("SELECT COUNT(*) FROM " + User.TABLE_NAME)
+    int count();
 
- @Query("SELECT score from User where pseudo=(:pseudo)")
- int scoredb(String pseudo);
+    @Query("SELECT score from User where pseudo=(:pseudo)")
+    int scoredb(String pseudo);
 
- @Query("SELECT * from User ORDER BY score DESC LIMIT 9")
- List<User>getAllUsers();
+    @Query("SELECT * from User ORDER BY score DESC LIMIT 9")
+    List<User> getAllUsers();
 
- @Query("SELECT pseudo from User")
- List<User>getAllUsersReg();
+    @Query("SELECT * FROM " + User.TABLE_NAME)
+    List<User> getAllUsersReg();
+ 
+    @Query("UPDATE User SET score = :score WHERE pseudo=(:pseudo)")
+    void updateScore(String pseudo, int score);
 
+    @Insert
+    void instarAll(User... users);
 
- @Query("UPDATE User SET score = :score WHERE pseudo=(:pseudo)")
- void updateScore(String pseudo, int score);
+    @Insert
+    void registerUser(User user);
 
- @Insert
- void instarAll(User ... users);
-
- @Insert
- void registerUser(User user);
-
- @Query("SELECT * from User where pseudo=(:pseudo) and password=(:password)")
-User login(String pseudo, String password);
-
+    @Query("SELECT * from User where pseudo=(:pseudo) and password=(:password)")
+    User login(String pseudo, String password);
 
 
- @Query("DELETE FROM " + User.TABLE_NAME + " WHERE " + User.COLUMN_ID + " = :id")
- int deleteById(long id);
+    @Query("DELETE FROM " + User.TABLE_NAME + " WHERE " + User.COLUMN_ID + " = :id")
+    int deleteById(long id);
 
- @Update
- int updateEntity(User obj);
+    @Update
+    int updateEntity(User obj);
 
- @Insert
- long insert(User users);
+    @Insert
+    long insert(User users);
 
 }
